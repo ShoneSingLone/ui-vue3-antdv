92f8eabfba85c8852c75920513317d4194328589
xiangxinglong <xinglong.xiang@gh-ca.com>
2022-11-15 11:35:47 +0800

    update demo

--- src/ui/xButton/DemoXButton.vue
+++ src/ui/xButton/DemoXButton.vue
@@ -1,12 +1,4 @@
-<template>
-	<xButton :configs="configs.btn.query" />
-	<xButton :configs="configs.btn.refresh" />
-	<xButton :configs="configs.btn.save" />
-	<xButton :configs="configs.btn.upload" />
-	<xButton :configs="configs.btn.delete" />
-</template>
-
-<script>
+<script lang="tsx">
 import {
 	defCol,
 	defColActions,
@@ -30,15 +22,31 @@ async function log() {
 }
 
 export default {
+	mounted() {
+		this.timer = setInterval(() => {
+			this.count++;
+		}, 1000);
+	},
+	beforeUnmount() {
+		clearInterval(this.timer);
+	},
 	data() {
+		const vm = this;
 		return {
+			timer: null,
+			count: 0,
 			configs: {
 				btn: {
-					modifyIP: {
+					normal: {
 						text: () => {
-							return $t("编辑").label;
+							return vm.$t("编辑").label + vm.count;
 						},
-						async onClick() {}
+						disabled() {
+							return vm.count % 3 === 0;
+						},
+						async onClick() {
+							await _.sleep(1000);
+						}
 					},
 					query: { preset: "query", onClick: log },
 					refresh: { preset: "refresh", onClick: log },
@@ -48,6 +56,74 @@ export default {
 				}
 			}
 		};
+	},
+	render() {
+		return (
+			<div>
+				<xButton configs={this.configs.btn.normal} />
+				<xButton configs={this.configs.btn.query} />
+				<xButton configs={this.configs.btn.refresh} />
+				<xButton configs={this.configs.btn.save} />
+				<xButton configs={this.configs.btn.upload} />
+				<xButton configs={this.configs.btn.delete} />
+				<mkit
+					md={`
+\`\`\`js
+<xButton configs={this.configs.btn.query} />
+<xButton configs={this.configs.btn.refresh} />
+<xButton configs={this.configs.btn.save} />
+<xButton configs={this.configs.btn.upload} />
+<xButton configs={this.configs.btn.delete} />
+
+
+async function log() {
+	const text = this.text;
+	this.text = "loading...";
+	await _.sleep(1000);
+	this.text = text;
+}
+
+mounted() {
+		this.timer = setInterval(() => {
+			this.count++;
+		}, 1000);
+	},
+beforeUnmount() {
+	clearInterval(this.timer);
+},
+data() {
+	const vm = this;
+	return {
+		timer: null,
+		count: 0,
+		configs: {
+			btn: {
+				normal: {
+					text: () => {
+						return vm.$t("编辑").label + vm.count;
+					},
+					disabled() {
+						return vm.count % 3 === 0;
+					},
+					async onClick() {
+						await _.sleep(1000);
+					}
+				},
+				query: { preset: "query", onClick: log },
+				refresh: { preset: "refresh", onClick: log },
+				save: { preset: "save", onClick: log },
+				upload: { preset: "upload", onClick: log },
+				delete: { preset: "delete", onClick: log }
+			}
+		}
+	};
+},
+
+\`\`\`
+`}
+				/>
+			</div>
+		);
 	}
 };
 </script>
