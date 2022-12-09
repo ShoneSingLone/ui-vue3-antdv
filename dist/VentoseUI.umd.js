06508125441d1bc73168b61d9cf21bfa0391113f
ShoneSingLone <ShoneSingLone@gmail.com>
2022-12-09 22:30:50 +0800

    publish

--- dist/VentoseUI.umd.js
+++ dist/VentoseUI.umd.js
@@ -29539,18 +29539,18 @@ div[id^=lazy-svg_] {
   z-index: 4;
   text-align: center;
 }
-.table-options {\r
-	display: flex;\r
-	flex-flow: row nowrap;\r
-	align-items: center;\r
+.table-options {
+	display: flex;
+	flex-flow: row nowrap;
+	align-items: center;
 	padding: 10px 0;
 }
-.table-filter {\r
+.table-filter {
 	margin-left: 4px;
 }
-.table-pagination {\r
+.table-pagination {
 	padding: 10px 0;
-}\r
+}
 .vir-item-component {
   height: 100%;
   overflow: auto;
