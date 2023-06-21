a62a78b7ac6159f7a591fbcd291cfa93911a6bd5
ShoneSingLone <ShoneSingLone@gmail.com>
2023-06-21 18:02:46 +0800

    feat(layer):data-tips

--- public/assets/html/layer/test/demo.js
+++ public/assets/html/layer/test/demo.js
@@ -1,37 +1,109 @@
+/* http://127.0.0.1:3011/public/assets/html/layer/test/demo.html */
 $(function () {
-    //关于
-
-    $(document).on("click", "[v-tips]", function (ele) {
-        try {
-            var $ele = $(this);
-            var width = $ele.width();
-            var $child = $('<span/>').text($ele.text()).css({ opacity: 0, position: "absolute" }).appendTo($("body"));
-            var widthChild = $child.width();
-            $child.remove();
-            if (width < widthChild) {
-                layer.tips($ele.text(), this, {
-                    tips: 1
-                });
-            }
-        } catch (e) {
-            console.error(e);
+    var inArea = {};
+    var timer = {};
+    function getContentTextWidth($ele) {
+        var $child = $('<span/>').text($ele.text()).css({ opacity: 0, position: "absolute" }).appendTo($("body"));
+        var widthChild = $child.width();
+        $child.remove();
+        return widthChild;
+    }
+    function closeTips(vlayerIndex) {
+        /* 标记清除 */
+        delete inArea[vlayerIndex];
+        if (timer[vlayerIndex]) {
+            clearTimeout(timer[vlayerIndex]);
         }
-
-    });
-
-    $("#about").on("click", function () {
-        //页面一打开就执行，放入ready是为了layer所需配件（css、扩展模块）加载完毕
-        layer.ready(function () {
-            layer.open({
-                type: 2,
-                title: "欢迎页",
-                maxmin: true,
-                area: ["800px", "500px"],
-                content: "/",
-                end: function () {
-                    layer.tips("Hi", "#about", { tips: 1 });
+        timer[vlayerIndex] = setTimeout(() => {
+            if (inArea[vlayerIndex]) {
+                return;
+            } else {
+                layer.close(vlayerIndex);
+                delete timer[vlayerIndex];
+            }
+        }, 200);
+    }
+    function enter(vlayerIndex) {
+        inArea[vlayerIndex] = true;
+    }
+    $(document).on(
+        "mouseenter.layer_tips",
+        "[data-tips]",
+        function (event) {
+            /* tips enter */
+            try {
+                var $ele = $(this);
+                var vlayerIndex = ($ele.attr("data-tips"));
+                if (timer[vlayerIndex]) {
+                    enter(vlayerIndex);
+                    clearTimeout(timer[vlayerIndex]);
+                    return;
                 }
-            });
-        });
-    });
+                var width = $ele.width();
+                var widthChild = getContentTextWidth($ele);
+                if (width < widthChild) {
+                    var tipsConfigs = {
+                        tips: [1, "#fff"],
+                        time: 1000 * 60 * 1
+                    };
+                    // var tipsContent = `<div style="overflow:auto;max-height:500px;" class="beautiful-scroll">${$ele.text()}</div>`;
+                    var tipsContent = $ele.text();
+                    vlayerIndex = layer.tips(tipsContent, this, tipsConfigs);
+                    enter(vlayerIndex);
+                    $ele.attr("data-tips", vlayerIndex);
+                }
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+    );
+    $(document).on(
+        "mouseenter.layer_tips",
+        ".layui-layer.layui-layer-tips[id^=layui-layer]",
+        function (event) {
+            /* content enter */
+            try {
+                var $content = $(this);
+                var contentId = $content.attr("id");
+                var vlayerIndex = contentId.match(/\d/)[0];
+                enter(vlayerIndex);
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+    );
+    /*  */
+    $(document).on(
+        "mouseleave.layer_tips",
+        "[data-tips]",
+        function (event) {
+            /* leave tips */
+            try {
+                var $ele = $(this);
+                var vlayerIndex = ($ele.attr("data-tips"));
+                if (vlayerIndex > -1) {
+                    closeTips(vlayerIndex);
+                }
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+    );
+    $(document).on(
+        "mouseleave.layer_tips",
+        ".layui-layer.layui-layer-tips[id^=layui-layer]",
+        function (event) {
+            /* leave content */
+            try {
+                var $content = $(this);
+                var contentId = $content.attr("id");
+                var vlayerIndex = (contentId.match(/\d/)[0]);
+                closeTips(vlayerIndex);
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+    );
 });
+
+
